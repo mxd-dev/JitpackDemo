a90bd5f307d601221f891d3311b83dae6095ba9f
mengxiangda <3213733489@qq.com>
2020-04-22 17:07:19 +0800

    Initial commit 首次提交

---
+++ utils/src/main/java/com/example/utils/LogUtils.java
@@ -0,0 +1,13 @@
+package com.example.utils;
+
+import android.util.Log;
+
+public class LogUtils {
+
+    private static final String TAG = "LogUtils";
+
+    public static void logD(String logStr){
+        Log.d(TAG, logStr);
+    }
+
+}
